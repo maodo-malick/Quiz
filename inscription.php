784236dc72237e8e6913c3067e15ad080080fd28
maodo-malick <62646925+maodo-malick@users.noreply.github.com>
2020-04-17 05:53:47 +0000

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style_joueur.css">
+</head>
+<body>
+<div id="table">
+       <div id="debut">
+        <div id="quiz">
+        <img src="quiz.jpeg "alt="bonhomme">
+        </div>
+        <h3>Le plaisir de jouer</h3>
+       </div>
+       <div id="formulaire">
+           <div id="scinder">
+           <div id="left">
+               <div id="text">
+                   <h1>S'INSCRIRE</h1>
+                   <p>pour tester le niveau de votre culture generale</p>
+               </div>
+               <div id="input">
+                   <p>Prenom</p>
+                   <input type="text" name="prenom">
+                   <br>
+                   <p>Nom</p>
+                   <input type="text" name="nom">
+                   <br />
+                   <p>Login</p>
+                   <input type="text" name="login">
+                   <br/>
+                   <p>Password</p>
+                   <input type="password" name="password">
+                   <p>Confirm Password</p>
+                   <input type="password" name="confirm">
+                   <input type="submit" name="compte" value="Créer un compte ">
+                   <input type="file" name="choix" value="Choisir un fichier">
+               </div>
+           </div>
+           <div id="right">
+           <div id="forpicture" name="picture"><p>Acceuillir une photo</p></div>
+           </div>
+        </div>
+ </div>
+</body>
+</html>
---
+++ sinscrire.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inscription.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="formulaire">
+           <div id="scinder">
+           <div id="left">
+               <div id="text">
+                   <h4>S'INSCRIRE</h4>
+                   <p>pour tester le niveau de votre culture generale</p>
+               </div><br>
+               <div id="input">
+                   <p>Prenom</p>
+                   <input type="text" name="prenom">
+                   <br>
+                   <p>Nom</p>
+                   <input type="text" name="nom">
+                   <br />
+                   <p>Login</p>
+                   <input type="text" name="login">
+                   <br/>
+                   <p>Password</p>
+                   <input type="password" name="password">
+                   <p>Confirm Password</p>
+                   <input type="password" name="confirm">
+                   <input type="submit" name="compte" value="Créer un compte ">
+                   <input type="file" name="choix" value="Choisir un fichier">
+               </div>
+           </div>
+           <div id="right">
+           <div id="forpicture" name="picture"><p>Acceuillir une photo</p></div>
+           </div>
+        </div>
+ </div>
+</body>
+</html>
+</body>
+</html>
